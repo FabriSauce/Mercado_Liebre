5c9f1a1df48890c0f3d3bb577f4009e220af4b7c
fabrisauce <fabriciosaucedo.fs@gmail.com>
2022-05-26 14:07:40 -0300

    [ add: app.js, style.css, register.css, /images, /views]

---
+++ app.js
@@ -0,0 +1,27 @@
+const express= require('express');
+const app= express();
+const path=require("path");
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}))
+
+const directory=path.join(__dirname,"/public");
+
+app.listen(3030,()=>{
+    console.log("funcionando")
+});
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+})
+
+app.get("/Login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+})
+app.get("/CrearCuenta",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/registro.html"))
+})
+
+app.post('/datos', (req,res) =>{
+    res.send(req.body)
+})
